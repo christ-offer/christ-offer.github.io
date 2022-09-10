e68826eea379c0ccc5dab4e18ea048f332c79210
Christoffer Holst <cholst@gmail.com>
2022-09-10 20:35:35 +0200

    fixed deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue()],
+  output: "server",
+  adapter: deno()
 });
